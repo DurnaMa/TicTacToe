d212f5dba9e8233da2b3276442738d2c659ccdf0
Mahir Durna <durna.m@outlook.de>
2024-04-26 21:17:49 +0200

    grundstruktur

---
+++ script.js
@@ -0,0 +1,21 @@
+let fieldes = [null, null, null, null, null, null, null, null, null];
+
+function init(){
+    render();
+}
+
+function render() {
+    let contentDiv = document.getElementById("content");
+    let tableHtml = "<table>";
+    for (let i = 0; i < 3; i++) {
+        tableHtml += "<tr>";
+        for (let j = 0; j < 3; j++) {
+            let index = i * 3 + j;
+            let symbol = fieldes[index] ? fieldes[index] : "";
+            tableHtml += `<td>${symbol}</td>`;
+        }
+        tableHtml += "</tr>";
+    }
+    tableHtml += "</table>";
+    contentDiv.innerHTML = tableHtml;
+}
